3c86698e822495f24cda0ed9bd7ccecd0cfa8ba2
mithleshs <heyitzmithlesh@gmail.com>
2016-12-30 18:03:51 +0530

    Done

--- src/part_01_linkedList/DoublyLinkedList.java
+++ src/part_01_linkedList/DoublyLinkedList.java
@@ -6,35 +6,57 @@ public class DoublyLinkedList {
 	int size = 0;
 
 	public static void main(String[] args) {
-
 		DoublyLinkedList list = new DoublyLinkedList();
 		list.add(12);
 		list.add(13);
 		list.add(1);
 		list.add(14);
 		list.add(2);
-
+		list.addToLast(21);
+		
 		printList(list);
 	}
 
-	public void add(int data) {
-
-		DNode node = new DNode(data);
-		node.next = head;
-		node.prev = null;
-		if (head != null)
-			head.prev = node;
-		head=node;
+	public void addToLast(int data) {
+		DNode node = new DNode(null, null, data);
+		if (head != null) {
+			DNode last = last();
+			node.prev = last;
+			last.next = node;
+		} else {
+			head = node;
+		}
 		size++;
 	}
 
-	public static void printList(DoublyLinkedList list) {
-
-		DNode tempHead = list.head;
-		while (tempHead != null) {
-			System.out.print(tempHead.data + " ");
+	public DNode last() {
+		DNode tempHead = head;
+		if (tempHead == null)
+			return null;
+		while (tempHead != null && tempHead.next != null) {
 			tempHead = tempHead.next;
 		}
+		return tempHead;
+	}
+
+	public DNode add(int data) {
+		DNode node = new DNode(head, null, data);
+		if (head != null)
+			head.prev = node;
+		head = node;
+		size++;
+
+		return node;
+	}
+
+	public static void printList(DoublyLinkedList list) {
+		DNode tempHead = list.head;
+		System.out.print("NULL ->");
+		while (tempHead != null) {
+			System.out.print(tempHead.data + " ->");
+			tempHead = tempHead.next;
+		}
+		System.out.print("NULL");
 		System.out.println("");
 	}
 }
