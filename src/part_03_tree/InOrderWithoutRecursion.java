e524172dec93cf745a089c692b0cc6c2e16d0178
msingh <msingh@CCI-Msingh-L.ccrn.com>
2017-01-12 16:51:58 +0530

    Added Description

--- src/part_03_tree/InOrderWithoutRecursion.java
+++ src/part_03_tree/InOrderWithoutRecursion.java
@@ -1,5 +1,16 @@
 package part_03_tree;
 
+/**
+ 1) Create an empty stack S.
+ 2) Initialize current node as root
+ 3) Push the current node to S and set current = current->left until current is NULL
+ 4) If current is NULL and stack is not empty then 
+	 a) Pop the top item from stack.
+	 b) Print the popped item, set current = popped_item->right 
+	 c) Go to step 3.
+ 5) If current is NULL and stack is empty then we are done.
+ **/
+
 import java.util.Stack;
 
 public class InOrderWithoutRecursion {
@@ -27,7 +38,7 @@ public class InOrderWithoutRecursion {
 			System.out.print(current.data + " ");
 			if (current.right != null) {
 				current = current.right;
-				
+
 				while (current != null) {
 					stack.push(current);
 					current = current.left;
