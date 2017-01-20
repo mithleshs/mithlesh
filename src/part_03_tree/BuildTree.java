df0c7e0bcc9f563c309ba54d055655dfe1c22309
msingh <msingh@CCI-Msingh-L.ccrn.com>
2017-01-20 17:52:23 +0530

    Construct Full Binary Tree from given preorder and postorder traversals http://www.geeksforgeeks.org/full-and-complete-binary-tree-from-given-preorder-and-postorder-traversals/

--- src/part_03_tree/BuildTree.java
+++ src/part_03_tree/BuildTree.java
@@ -3,18 +3,37 @@ package part_03_tree;
 public class BuildTree {
 
 	static int preIndex = 0;
-
+	static int fullPreIndex = 0;
 	public static void main(String[] args) {
 
 		BinaryTree tree = new BinaryTree();
 		int inPath[] = new int[] { 1, 2, 3, 4, 5, 6 };
 		int prePath[] = new int[] { 4, 2, 1, 3, 6, 5 };
 		int end = inPath.length;
-		tree.root = buildTree(inPath, prePath, 0, end-1);
+		tree.root = buildTree(inPath, prePath, 0, end - 1);
 
 		// building the tree by printing inorder traversal
 		System.out.println("Inorder traversal of constructed tree is : ");
 		tree.inorder(tree.root);
+
+		System.out
+				.println("OutPut to Build Full Binary Tree...here below start");
+
+		int pre[] = { 1, 2, 4, 8, 9, 5, 3, 6, 7 };
+		int post[] = { 8, 9, 4, 5, 2, 6, 7, 3, 1 };
+		int size = pre.length;
+
+		BinaryTree fullTree = new BinaryTree();
+
+		fullTree.root = buildFullBinaryTree(pre, post, 0, 0, size - 1, size);
+
+		System.out.println("PRE-ORDER");
+		fullTree.preorder(fullTree.root);
+		System.out.println("");
+
+		System.out.println("Post-ORDER");
+		fullTree.postorder(fullTree.root);
+		System.out.println("");
 
 	}
 
@@ -48,6 +67,43 @@ public class BuildTree {
 			}
 		}
 		return inOrderIndex;
+	}
+
+	/*
+	 * A Full Binary Tree is a binary tree where every node has either 0 or 2
+	 * children
+	 */
+	public static TreeNode buildFullBinaryTree(int[] pre, int[] post,
+			int fullPreIndex, int lowPostIndex, int highPostIndex, int size) {
+
+		// Base case
+		if (fullPreIndex >= size || lowPostIndex > highPostIndex) {
+			return null;
+		}
+		TreeNode root = new TreeNode(pre[fullPreIndex]);
+		fullPreIndex++;
+
+		// If the current subarry has only one element, no need to recur
+		if (lowPostIndex == highPostIndex)
+			return root;
+
+		// Search the next element of pre[] in post[]
+		int i;
+		for (i = lowPostIndex; i <= highPostIndex; i++)
+			if (pre[fullPreIndex] == post[i])
+				break;
+
+		// Use the index of element found in postorder to divide postorder array
+		// in
+		// two parts. Left subtree and right subtree
+		if (i <= highPostIndex) {
+			root.left = buildFullBinaryTree(pre, post, fullPreIndex, lowPostIndex,
+					i, size);
+			root.right = buildFullBinaryTree(pre, post, fullPreIndex, i + 1,
+					highPostIndex, size);
+		}
+
+		return root;
 	}
 
 }
