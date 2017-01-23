c00b7038940d963b9e6d3edf99a19e0360e087b2
msingh <msingh@CCI-Msingh-L.ccrn.com>
2017-01-23 12:47:51 +0530

    Binary Search Tree | Set 2 (Delete) http://quiz.geeksforgeeks.org/binary-search-tree-set-2-delete/

--- src/part_03_tree/searching/BinarySearchTree.java
+++ src/part_03_tree/searching/BinarySearchTree.java
@@ -30,11 +30,31 @@ public class BinarySearchTree {
 		} else {
 			System.out.println("Not Found 404");
 		}
-		
-		BinaryTree tree=new BinaryTree();
+
+		BinaryTree tree = new BinaryTree();
 		System.out.println("IN-ORDER");
 		tree.inorder(bst.root);
 		System.out.println("");
+
+		System.out.println("DELETE A NODE");
+
+		System.out.println("Inorder traversal of the given tree");
+        tree.inorder(bst.root);
+ 
+        System.out.println("\nDelete 20");
+        bst.deleteKey(20);
+        System.out.println("Inorder traversal of the modified tree");
+        tree.inorder(bst.root);
+        
+        System.out.println("\nDelete 30");
+        bst.deleteKey(30);
+        System.out.println("Inorder traversal of the modified tree");
+        tree.inorder(bst.root);
+        
+        System.out.println("\nDelete 50");
+        bst.deleteKey(50);
+        System.out.println("Inorder traversal of the modified tree");
+        tree.inorder(bst.root);
 	}
 
 	public void insert(int data) {
@@ -67,4 +87,49 @@ public class BinarySearchTree {
 			return search(node.left, data);
 		}
 	}
+
+	void deleteKey(int key) {
+		root = deleteRec(root, key);
+	}
+
+	int minValue(TreeNode root) {
+		int minv = root.data;
+		while (root.left != null) {
+			minv = root.left.data;
+			root = root.left;
+		}
+		return minv;
+	}
+
+	TreeNode deleteRec(TreeNode root, int key) {
+		/* Base Case: If the tree is empty */
+		if (root == null)
+			return root;
+
+		/* Otherwise, recur down the tree */
+		if (key < root.data)
+			root.left = deleteRec(root.left, key);
+		else if (key > root.data)
+			root.right = deleteRec(root.right, key);
+
+		// if key is same as root's key, then This is the node
+		// to be deleted
+		else {
+			// node with only one child or no child
+			if (root.left == null)
+				return root.right;
+			else if (root.right == null)
+				return root.left;
+
+			// node with two children: Get the inorder successor (smallest
+			// in the right subtree)
+			root.data = minValue(root.right);
+
+			// Delete the inorder successor
+			root.right = deleteRec(root.right, root.data);
+		}
+
+		return root;
+	}
+
 }
