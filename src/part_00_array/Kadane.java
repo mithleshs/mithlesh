045e136a5e81c5420b42ab0dbc54d01c536d1c46
DM <DM@DM-PC>
2017-05-05 10:39:05 +0530

    Largest Sum Contiguous Subarray

--- src/part_00_array/Kadane.java
+++ src/part_00_array/Kadane.java
@@ -1,22 +1,51 @@
 package part_00_array;
 
+/*Largest Sum Contiguous Subarray
+ Write an efficient C program to find the sum of contiguous subarray within a one-dimensional array of numbers which has the largest sum.
+ */
 public class Kadane {
 
 	public static void main(String[] args) {
 
 		int[] temp = { -2, -3, 4, -1, -2, 1, 5, -3 };
+
+		int maxSubArrSum = maxSubArraySum(temp);
+		System.out.println(maxSubArrSum);
 	}
 
-	public static void maxSubArraySum(int[] temp) {
+	/*
+	 * Kadane’s Algorithm:
+	 * 
+	 * Initialize: max_so_far = 0 max_ending_here = 0
+	 * 
+	 * Loop for each element of the array (a) max_ending_here = max_ending_here
+	 * + a[i] (b) if(max_ending_here < 0) max_ending_here = 0 (c) if(max_so_far
+	 * < max_ending_here) max_so_far = max_ending_here return max_so_far
+	 */
+
+	public static int maxSubArraySum(int[] temp) {
 		int max_so_far = 0;
 		int max_ending_here = 0;
+		int startIn=0;
+		int endIn=0;
+		int s=0;
 		for (int i = 0; i < temp.length; i++) {
 
-			max_ending_here+=temp[i];
-			if(max_ending_here>max_so_far){
-				
+			max_ending_here += temp[i];
+			if (max_ending_here > max_so_far) {
+				max_so_far = max_ending_here;
+				startIn=s;
+				endIn=i;
+			}
+
+			if (max_ending_here < 0) {
+				max_ending_here = 0;
+				s=i+1;
 			}
 		}
+		
+		System.out.println("SUBARRAY is from "+startIn+" to "+endIn);
+		return max_so_far;
 	}
 
 }
