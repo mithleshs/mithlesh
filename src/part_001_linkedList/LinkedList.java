084d349a249a90e10b82539bc621ba052fa2006f
DM <DM@DM-PC>
2017-05-24 11:13:31 +0530

    Pairwise swap elements of a given linked list

--- src/part_001_linkedList/LinkedList.java
+++ src/part_001_linkedList/LinkedList.java
@@ -28,19 +28,20 @@ public class LinkedList {
 		list.printList();
 		list.deleteNode(list.head.next.next);
 		list.printList();
-		//list.deleteNodeAt(2);
-		//list.printList();
-		//list.size();
-		//System.out.println("Recursive size is : " + list.getSize(list.head));
-		//list.swapNodes(2, 1);
-		//list.printList();
-		//System.out.println("Middle node is : "+list.middleNode().data);
-		//System.out.println("Nth Node from End is : "+list.findNodeFromEnd(4).data);
-		
-		//list.reverseUtil(list.head, null);
-		//list.printList();
-		//mergeSortedList();
-		removeDuplicateFromSortedList();
+		// list.deleteNodeAt(2);
+		// list.printList();
+		// list.size();
+		// System.out.println("Recursive size is : " + list.getSize(list.head));
+		// list.swapNodes(2, 1);
+		// list.printList();
+		// System.out.println("Middle node is : "+list.middleNode().data);
+		// System.out.println("Nth Node from End is : "+list.findNodeFromEnd(4).data);
+
+		// list.reverseUtil(list.head, null);
+		// list.printList();
+		// mergeSortedList();
+		// removeDuplicateFromSortedList();
+		pairWiseSwap();
 	}
 
 	public void printList() {
@@ -193,132 +194,152 @@ public class LinkedList {
 		currentY.next = tempX;
 
 	}
-	
-	public Node middleNode(){
-		
-		if(head==null)
+
+	public Node middleNode() {
+
+		if (head == null)
 			return head;
-		Node fastPtr=head;
-		Node slowPtr=head;
-		while(fastPtr!=null&&fastPtr.next!=null){
-			slowPtr=slowPtr.next;
-			fastPtr=fastPtr.next.next;
+		Node fastPtr = head;
+		Node slowPtr = head;
+		while (fastPtr != null && fastPtr.next != null) {
+			slowPtr = slowPtr.next;
+			fastPtr = fastPtr.next.next;
 		}
-		
+
 		return slowPtr;
 	}
-	
-	public Node findNodeFromEnd(int pos){
-		Node firstPtr=head;
-		Node secondPtr=head;
-		int count=1;
-		
-		while(firstPtr!=null && secondPtr!=null){
-			if(count>pos){
-				secondPtr=secondPtr.next;
+
+	public Node findNodeFromEnd(int pos) {
+		Node firstPtr = head;
+		Node secondPtr = head;
+		int count = 1;
+
+		while (firstPtr != null && secondPtr != null) {
+			if (count > pos) {
+				secondPtr = secondPtr.next;
 			}
-			
-			firstPtr=firstPtr.next;
+
+			firstPtr = firstPtr.next;
 			count++;
 		}
-		
+
 		return secondPtr;
 	}
-	
-	public void reverse(){
-		Node current=head;
-		Node prev=null;
-		Node next=null;
-		
-		while(current!=null){
+
+	public void reverse() {
+		Node current = head;
+		Node prev = null;
+		Node next = null;
+
+		while (current != null) {
 			next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
+			current.next = prev;
+			prev = current;
+			current = next;
 		}
-		
-		head=prev;
+
+		head = prev;
 	}
-	
-	public Node reverseUtil(Node current,Node prev){
-		
-		if(current.next==null){
-			head=current;
-			head.next=prev;
-			
+
+	public Node reverseUtil(Node current, Node prev) {
+
+		if (current.next == null) {
+			head = current;
+			head.next = prev;
+
 			return head;
 		}
-		
-		Node next1=current.next;
-		current.next=prev;
+
+		Node next1 = current.next;
+		current.next = prev;
 		reverseUtil(next1, current);
-		
+
 		return head;
 	}
-	
-	public static Node merge(Node a,Node b){
-		Node newNode=null;
-		if(a==null)
-		{
+
+	public static Node merge(Node a, Node b) {
+		Node newNode = null;
+		if (a == null) {
 			return b;
-		}
-		else if(b==null){
+		} else if (b == null) {
 			return a;
-		}
-		else if(a.data>=b.data){
-			newNode=b;
-			newNode.next=merge(a,b.next);
-		}
-		else if(a.data<=b.data){
-			newNode=a;
-			newNode.next=merge(a.next,b);
+		} else if (a.data >= b.data) {
+			newNode = b;
+			newNode.next = merge(a, b.next);
+		} else if (a.data <= b.data) {
+			newNode = a;
+			newNode.next = merge(a.next, b);
 		}
 		return newNode;
-		
+
 	}
-	
-	public static void mergeSortedList(){
-		LinkedList list1=new LinkedList();
-		list1.head=new Node(5);
-		list1.head.next=new Node(10);
-		list1.head.next.next=new Node(15);
-		
-		LinkedList list2=new LinkedList();
-		list2.head=new Node(2);
-		list2.head.next=new Node(3);
-		list2.head.next.next=new Node(20);
-		
+
+	public static void mergeSortedList() {
+		LinkedList list1 = new LinkedList();
+		list1.head = new Node(5);
+		list1.head.next = new Node(10);
+		list1.head.next.next = new Node(15);
+
+		LinkedList list2 = new LinkedList();
+		list2.head = new Node(2);
+		list2.head.next = new Node(3);
+		list2.head.next.next = new Node(20);
+
 		list1.printList();
 		list2.printList();
-		
-		list1.head=merge(list1.head, list2.head);
+
+		list1.head = merge(list1.head, list2.head);
 		list1.printList();
-		
+
+	}
+
+	public static void removeDuplicateFromSortedList() {
+
+		LinkedList list = new LinkedList();
+		list.head = new Node(11);
+		list.head.next = new Node(11);
+		list.head.next.next = new Node(11);
+		list.head.next.next.next = new Node(12);
+		list.head.next.next.next.next = new Node(12);
+		list.head.next.next.next.next.next = new Node(14);
+		Node current = list.head;
+		Node next = null;
+		list.printList();
+		while (current != null) {
+			next = current.next;
+			if (next != null && current.data == next.data) {
+				current.next = next.next;
+			} else
+				current = current.next;
+
+		}
+
+		list.printList();
+
+	}
+
+	public static void pairWiseSwap() {
+		LinkedList list = new LinkedList();
+		list.head = new Node(1);
+		list.head.next = new Node(2);
+		list.head.next.next = new Node(3);
+		list.head.next.next.next = new Node(4);
+		list.head.next.next.next.next = new Node(5);
+		list.head.next.next.next.next.next = new Node(6);
+		list.printList();
+
+		Node current = list.head;
+		int temp = 0;
+		while (current != null && current.next != null) {
+			temp = current.next.data;
+			current.next.data=current.data;
+			current.data=temp;
+			current = current.next.next;
+		}
+
+		list.printList();
+
 	}
 	
-	public static void removeDuplicateFromSortedList(){
-		
-		LinkedList list=new LinkedList();
-		list.head=new Node(11);
-		list.head.next=new Node(11);
-		list.head.next.next=new Node(11);
-		list.head.next.next.next=new Node(12);
-		list.head.next.next.next.next=new Node(12);
-		list.head.next.next.next.next.next=new Node(14);
-		Node current=list.head;
-		Node next=null;
-		list.printList();
-		while(current!=null  ){
-			next=current.next;
-			if(next!=null && current.data==next.data){
-				current.next=next.next;
-			}
-			else
-				current=current.next;
-			
-		}
-		
-		list.printList();
-		
-	}
+	
 }
