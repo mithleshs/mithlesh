106bf01dd57dff6d6827bf32e0f895e73bf03442
DM <DM@DM-PC>
2017-05-30 10:37:08 +0530

    Made changes in selection sort

--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -3,9 +3,9 @@ package sorting;
 /**
  * Logic (Worst-case and average complexity : О(n2))
  * 
- * @1. currentPosToSort=foundMinAt=0. scan from 0 to n-1 and find smallest index
- *     it will be foundMinAt swap arr[currentPosToSort]=arr[foundMinAt]
- * @2. now start again from next unsorted position currentPosToSort+1 and do
+ * @1. foundMinAt=0. scan from 0 to n-1 and find smallest index
+ *     it will be foundMinAt swap arr[startFrom]=arr[foundMinAt]
+ * @2. now start again from next unsorted position startFrom+1 and do
  *     above steps
  * 
  * */
@@ -18,23 +18,22 @@ public class SelectionSort {
 
 		int size = input.length;
 		print(input);
-		int currentPosToSort = 0;
-		int loopCount = 0;
-		while (currentPosToSort < size - 1) {
-			int foundMinAt = currentPosToSort;
-			for (int i = currentPosToSort + 1; i < size; i++) {
+		int startFrom = 0;
+		while (size>0) {
+			int foundMinAt = startFrom;
+			for (int i = startFrom + 1; i < input.length; i++) {
 				if (input[i] < input[foundMinAt]) {
 					foundMinAt = i;
 				}
-				loopCount++;
+				
 			}
-			if (foundMinAt != currentPosToSort) {
-				swapNumbers(currentPosToSort, foundMinAt, input);
+			if (foundMinAt != startFrom) {
+				swapNumbers(startFrom, foundMinAt, input);
 			}
-			currentPosToSort++;
+			startFrom++;
+			size--;
 		}
 		print(input);
-		System.out.println(loopCount);
 	}
 
 	public static void swapNumbers(int i, int j, int[] array) {
