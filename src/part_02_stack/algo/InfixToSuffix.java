9a337a872f830927ea60d7d22fc2141b9dbc71b5
msingh <msingh@CCI-Msingh-L.ccrn.com>
2017-06-16 14:50:06 +0530

    Description added

--- src/part_02_stack/algo/InfixToSuffix.java
+++ src/part_02_stack/algo/InfixToSuffix.java
@@ -12,6 +12,19 @@ public class InfixToSuffix {
 		System.out.println("The expression in postfix is:" + toPostfix(infix));
 	}
 
+	/**Algorithm
+	1. Scan the infix expression from left to right.
+	2. If the scanned character is an operand, output it.
+	3. Else,
+	…..3.1 If the precedence(scanned operator)> precedence(operator in the stack)(or the stack is empty) and stack top not is '(', push it.
+	…..3.2 Else, Pop the operator from the stack until the precedence of the scanned operator is less-equal to the precedence of the operator residing on the top of the stack. 
+	Push the scanned operator to the stack.
+	4. If the scanned character is an ‘(‘, push it to the stack.
+	5. If the scanned character is an ‘)’, pop and output from the stack until an ‘(‘ is encountered.
+	6. Repeat steps 2-6 until infix expression is scanned.
+	7. Pop and output from the stack until it is not empty.
+	**/
+	
 	private static String toPostfix(String infix) {
 		char symbol;
 		String postfix = "";
