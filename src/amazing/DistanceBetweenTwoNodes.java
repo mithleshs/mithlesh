a82762102ba7b34f02814bdb5f23f64a0db066f9
msingh <msingh@CCI-Msingh-L.ccrn.com>
2017-07-18 18:16:39 +0530

    Find the Distance between Two Nodes of a Binary Tree

---
+++ src/amazing/DistanceBetweenTwoNodes.java
@@ -0,0 +1,14 @@
+package amazing;
+
+/**
+ * Distance(X, Y) = Distance(root, X) + Distance(root, Y) — 2*Distance(root to
+ * LCA(X,Y)) where LCA(X,Y) = Lowest Common Ancestor of X and Y
+ */
+
+public class DistanceBetweenTwoNodes {
+
+	public static void main(String[] args) {
+
+	}
+
+}
